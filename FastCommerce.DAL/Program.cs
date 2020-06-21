67f7046795d722daa413f9308c7feb8cd15a64ff
M. UTKU KUL <m.utkukul@gmail.com>
2020-06-21 14:36:48 +0300

    Add project files.

---
+++ FastCommerce.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastCommerce.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FastCommerce.Entities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastCommerce.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
