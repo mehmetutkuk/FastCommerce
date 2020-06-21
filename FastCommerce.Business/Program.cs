87608523b8a235e8e0b7264c33b66b0e7b2d4122
M. UTKU KUL <m.utkukul@gmail.com>
2020-06-21 19:17:38 +0300

    gitflow-feature-stash: BusinessLayer

---
+++ FastCommerce.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastCommerce.Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
