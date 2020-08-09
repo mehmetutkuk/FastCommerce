3e4e19bc96e4ae7d8f51a72b0698ef3e82c04aa5
M. UTKU KUL <m.utkukul@gmail.com>
2020-08-09 17:29:47 +0300

    gitflow-feature-stash: JWTBearerToken

--- FastCommerce.Web.API/Startup.cs
+++ FastCommerce.Web.API/Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 using FastCommerce.Business.UserManager;
 using FastCommerce.DAL;
 using FastCommerce.Entities.Entities;
+using FastCommerce.Entities.Models;
+using FastCommerce.Web.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Localization;
@@ -14,6 +18,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Nest;
 using Utility.MailServices;
@@ -69,9 +74,60 @@ namespace FastCommerce.Web.API
                         Url = new Uri("https://github.com/mehmetutkuk/FastCommerce/blob/master/LICENSE"),
                     }
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer ASD123ASD123'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                  });
             });
             services.AddSwaggerGenNewtonsoftSupport();
 
+            services.Configure<TokenModel>(Configuration.GetSection("tokenManagement"));
+            var token = Configuration.GetSection("tokenManagement").Get<TokenModel>();
+            var secret = Encoding.ASCII.GetBytes(token.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(secret),
+                };
+            });
 
             //services.AddLocalization(opt => opt.ResourcesPath = "Resources");
             //services.Configure<RequestLocalizationOptions>(
@@ -107,6 +163,8 @@ namespace FastCommerce.Web.API
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -121,9 +179,9 @@ namespace FastCommerce.Web.API
             //var localizeOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
             //app.UseRequestLocalization(localizeOptions.Value);
         }
-
     }
-    public static class YourDomainDIExtensions
+
+    public static class DomainDIExtensions
     {
         public static void AddDomainDataServices(this IServiceCollection services)
         {
